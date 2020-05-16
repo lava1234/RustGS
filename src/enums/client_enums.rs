591e7d6ea3aee8c81ef9fef87adaca097f65e739
ug00031 <ug00031@surrey.ac.uk>
2020-05-16 20:22:48 +0100

    setting up enums

---
+++ src/enums/client_enums.rs
@@ -0,0 +1,5 @@
+enum ClientEnums {
+    PONG = 0x2001,
+    MOVE_REQUEST = 0x3000
+    FIGHT_REQUEST = 0x6000,
+}
---
+++ src/enums/server_enums.rs
@@ -0,0 +1,5 @@
+enum ServerEnums {
+    PING = 0x2000,
+    MOVE_RESPONSE = 0x3000,
+    FIGHT_RESPONSE = 0x6001,
+}
