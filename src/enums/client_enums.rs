00ba3ef2ee686ac643572b714d36537fab8b1d3d
ug00031 <ug00031@surrey.ac.uk>
2020-05-16 20:23:21 +0100

    typo

--- src/enums/client_enums.rs
+++ src/enums/client_enums.rs
@@ -1,4 +1,4 @@
-enum ClientEnums {
+enum ClientOpcodes {
     PONG = 0x2001,
     MOVE_REQUEST = 0x3000
     FIGHT_REQUEST = 0x6000,
--- src/enums/server_enums.rs
+++ src/enums/server_enums.rs
@@ -1,4 +1,4 @@
-enum ServerEnums {
+enum ServerOpcodes {
     PING = 0x2000,
     MOVE_RESPONSE = 0x3000,
     FIGHT_RESPONSE = 0x6001,
